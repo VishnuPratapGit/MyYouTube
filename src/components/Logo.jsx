fdd80d936daedc4ba72d492d6c72d1355d667fa8
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2025-01-04 12:43:45 +0530

    change logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,8 +4,8 @@ import { FaYoutube } from 'react-icons/fa';
 function Logo({ logo = "", text = "" }) {
     return (
         <div className="flex items-center gap-0.5 cursor-pointer">
-            <FaYoutube className={`text-red-500 ${logo}`} />
-            <div className={` mb-0.5 select-none font-oswald ${text}`}>YouTube</div>
+            <img src="/logoipsum.svg" className={`h-5 mr-2`}/>
+            <div className={` mb-0.5 select-none font-oswald ${text}`}>videoshare</div>
         </div>
     );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
         <div className='flex flex-col w-full items-center justify-center h-full'>
             <div className='border-2 p-5 rounded-2xl'>
                 <div className="mb-2 flex justify-center">
-                    <Logo logo='text-5xl' text='text-3xl' />
+                    <Logo text='text-3xl' />
                 </div>
                 <h2 className="text-center text-2xl font-roboto font-semibold mt-6">Login to your Account</h2>
                 <p className="mt-2 font-roboto text-center mb-6">Don&apos;t have account?&nbsp;
